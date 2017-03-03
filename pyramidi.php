52c99c6ff14b26929c3915061e8a8f3ba46d80fe
Samppa <samppa.sassi@gmail.com>
2017-03-03 11:40:40 +0200

    lisätty form

--- pyramidi.php
+++ pyramidi.php
@@ -1,3 +1,12 @@
+<html>
+<body>
+    <form action="" method="post">
+        <input type="number" name="numero">
+        <?php echo $_POST ?>
+        <input type="submit" value="RAKENNA!">
+    </form>
+</body>
+</html>
 <?php
 
 for($i=1;$i<=5;$i++){
