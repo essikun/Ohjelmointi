bc595002ecfdff3fbe67260b9e0db6856b5b11a5
Samppa <samppa.sassi@gmail.com>
2017-03-10 09:41:50 +0200

    uusi

--- pyramidi.php
+++ pyramidi.php
@@ -2,18 +2,14 @@
 <body>
     <form action="" method="post">
         <input type="number" name="numero">
-        <?php echo $_POST ?>
         <input type="submit" value="RAKENNA!">
     </form>
 </body>
 </html>
-<?php
-
-for($i=1;$i<=5;$i++){
-    for($x=1;$x<=$i;$x++){
-        echo "* ";
-    }
-    echo "<br />";
-}
-
-?>
+<?php if(!isset($_POST["numero"])){ 
+    for($_POST["numero"]<=10;$_POST["numero"]++;) {
+            for($x=1;$x<=$_POST["numero"];$x++){
+                echo "* ";
+            } echo "<br />";
+        }}
+        ?>
